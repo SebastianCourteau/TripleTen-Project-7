338a327f144496634c968f9ce28828c050d742e7
Sebastian Courteau <sebastiancourteau7575@gmail.com>
2024-07-11 18:11:47 -0400

    Updated project with updated URL in config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-e2174c83-a977-45d8-82ec-d8ceed899c5c.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-0e9bba2d-4941-4277-a684-06a4b9648db0.containerhub.tripleten-services.com"
 }
